864fe738df6f1f25a2c3916e0a7f26839198e831
AlexOfRivia <alexbhandari22@gmail.com>
2024-11-14 21:13:05 +0100

    More work on item widget

--- DepoPro/DepoPro.cpp
+++ DepoPro/DepoPro.cpp
@@ -1,6 +1,7 @@
 #include "DepoPro.h"
 #include <QLineEdit>
 #include <QTextEdit>
+#include <QCheckBox>
 #include <QListWidgetItem>
 
 void DepoPro::addNewItem()
@@ -11,19 +12,35 @@ void DepoPro::addNewItem()
     //Widget and widget layout
     QWidget* itemWidget = new QWidget();
     itemWidget->setLayout(new QHBoxLayout(itemWidget));
+    itemWidget->setStyleSheet("border:none; background-color:rgba(45,45,45,200);");
     
-    //Adding things to the item Widget
+    //Adding things to the item Widget:
+
+    //SpinBox
     QSpinBox* spinBox = new QSpinBox();
+    spinBox->setStyleSheet("max-width:50px; color: white;");
     
+    //Item text
     QTextEdit* itemName = new QTextEdit();
-    itemName->setStyleSheet("border:none; background-color:rgba(45,45,45,255);font: 700 14pt 'Arial';  color: white; max-height: 65px;");
+    itemName->setStyleSheet("border:none; background-color:rgba(45,45,45,255);font: 700 12pt 'Arial';  color: white; max-height: 65px; max-width:375px;");
+    
+    //Checkbox
+    QCheckBox* checkBox = new QCheckBox();
+    checkBox->setStyleSheet("max-width:25px;background-color:rgba(45,45,45,200);");
+
+    //Frame (as a spacer)
+    QFrame* spacerFrame = new QFrame();
+    spacerFrame->setStyleSheet("width:130px;background-color:rgba(45,45,45,200);");
+    
+    itemWidget->layout()->addWidget(checkBox);
     itemWidget->layout()->addWidget(itemName);
+    itemWidget->layout()->addWidget(spacerFrame);
     itemWidget->layout()->addWidget(spinBox);
     listItem->setSizeHint(itemWidget->sizeHint());
 
-
+    //Setting the itemWidget as a listItem, so that can be put into a list
     ui.itemList->addItem(listItem);
-    ui.itemList->setItemWidget(listItem, itemWidget);
+    ui.itemList->setItemWidget(listItem, itemWidget); 
     
     
 }
