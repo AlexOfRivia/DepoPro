7b0ff983b8e810f62bad4c4f1fd501996b229883
Alexander Bhandari <31488157+AlexOfRivia@users.noreply.github.com>
2024-11-24 14:35:11 +0100

    Added initial loading stock items from a file

--- DepoPro/DepoPro.cpp
+++ DepoPro/DepoPro.cpp
@@ -37,8 +37,26 @@ void DepoPro::removeItem()
 void DepoPro::loadFromFile()
 {
     QString fileName = QFileDialog::getOpenFileName(this, "Please, chose a file to open");
-
     QFile file(fileName);
+    if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        // bind it
+        QTextStream in(&file);
+
+        while (!in.atEnd()) {
+            QString line = in.readLine(); //Creates a line of text from the txt file
+            QListWidgetItem* loadedListItem = new QListWidgetItem(); //Creates a new widget item
+            stockItem* loadedItem = new stockItem; //Creates a new stock item
+
+            loadedItem->itemName->setText(line); //Sets the stock item name as the read line
+            
+            loadedListItem->setSizeHint(loadedItem->stockItemWidget->sizeHint());
+
+            ui.itemList->addItem(loadedListItem); //Adds the item to the list
+            ui.itemList->setItemWidget(loadedListItem, loadedItem->stockItemWidget);
+        }
+
+        file.close(); //Closes the file
+    }
 }
 
 //Constructor
