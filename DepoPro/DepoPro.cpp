7cb9d657ab88e18b0c832272554d0d00e3e5dafb
Alexander Bhandari <31488157+AlexOfRivia@users.noreply.github.com>
2024-12-22 13:21:19 +0100

    Bug fixes and improvements

--- DepoPro/DepoPro.cpp
+++ DepoPro/DepoPro.cpp
@@ -5,6 +5,7 @@
 #include <QFileDialog>
 #include <QListWidgetItem>
 #include <QSaveFile>
+#include <QMessageBox>
 
 
 //Adding a new item to list
@@ -26,56 +27,83 @@ void DepoPro::addNewItem()
 //Removing an item from list
 void DepoPro::removeItem()
 {
-    if (ui.itemList->count()!=0)
+    int index = ui.itemList->currentRow(); //Get the index of the selected item
+    if (index != -1) //Check if an item is selected
     {
-        // Get the index of the selected item
-        int index = ui.itemList->currentRow(); //Getting the list item index
-
-        // Remove the item from the list and the stock vector
+        //Remove the item from the list and the stock vector
         QListWidgetItem* selectedItem = ui.itemList->takeItem(index); //Removing the selected item from the list
         this->stock.erase(this->stock.begin() + index); //Erasing the vector item
 
-        // Delete the selected item object
+        //Delete the selected item object
         delete selectedItem;
+	}
+    else
+    {
+		QMessageBox::warning(this, "Error", "No item selected."); //Return an error message if no item is selected
     }
 }
 
 //Loading a stock list to current list from file
 void DepoPro::loadFromFile()
 {
-    QString fileName = QFileDialog::getOpenFileName(this, "Please, chose a file to open"); //Opens a file dialog box
+    QString fileName = QFileDialog::getOpenFileName(this, "Please, choose a file to open"); //Opens a file dialog box
+    if (fileName.isEmpty()) 
+    {
+        return; //if no file is selected, return early
+    }
+
     QFile file(fileName);
-    if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
-        // bind it
-        QTextStream in(&file);
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) 
+    {
+        // Handle file open error
+        QMessageBox::warning(this, "Error", "Failed to open the file.");
+        return;
+    }
 
-        while (!in.atEnd()) {
-            QString line = in.readLine(); //Creates a line of text from the txt file
-            QStringList lineList = line.split(";"); //Splits the currently read line after ;
-            QString stockItemName = lineList.value(0); //Setting the item name to the first value in string list
-			QString stockItemPriceStr = lineList.value(1); //Setting the item name to the second value in string list
-            QString stockItemAmountStr = lineList.value(2); //Setting the item name to the third value in string list
-            int stockItemAmount = stockItemAmountStr.toInt(); //Converting the read amount to int
-			float stockItemPrice = stockItemPriceStr.toFloat(); //Converting the read price to int
+    QTextStream in(&file);
 
-            QListWidgetItem* loadedListItem = new QListWidgetItem(); //Creates a new widget item
-            stockItem* loadedItem = new stockItem; //Creates a new stock item
-
-            loadedItem->itemName->setText(stockItemName); //Sets the stock item name as the split name
-			loadedItem->priceSpinBox->setValue(stockItemPrice); //Sets the stock item price as the converted int value
-            loadedItem->spinBox->setValue(stockItemAmount); //Sets the stock item amount as the converted int value
-
-            this->stock.push_back(*loadedItem); //Adding a new element to a vector
-
-            loadedListItem->setSizeHint(loadedItem->stockItemWidget->sizeHint());
-
-            ui.itemList->addItem(loadedListItem); //Adds the item to the list
-            ui.itemList->setItemWidget(loadedListItem, loadedItem->stockItemWidget);
-            
+    while (!in.atEnd()) 
+    {
+        QString line = in.readLine(); //Creates a line of text from the txt file
+        QStringList lineList = line.split(";"); //Splits the currently read line after ;
+        if (lineList.size() < 3) 
+        {
+            //Handle parsing error
+            QMessageBox::warning(this, "Error", "Invalid line format in the file.");
+            continue;
         }
 
-        file.close(); //Closes the file
+        QString stockItemName = lineList.value(0); //Setting the item name to the first value in string list
+        QString stockItemPriceStr = lineList.value(1); //Setting the item price to the second value in string list
+        QString stockItemAmountStr = lineList.value(2); //Setting the item amount to the third value in string list
+
+        bool amountOk, priceOk;
+        int stockItemAmount = stockItemAmountStr.toInt(&amountOk); //Converting the read amount to int
+        float stockItemPrice = stockItemPriceStr.toFloat(&priceOk); //Converting the read price to float
+
+        if (!amountOk || !priceOk) 
+        {
+            //Handle conversion error
+            QMessageBox::warning(this, "Error", "Invalid stock amount or price in the file.");
+            continue;
+        }
+
+        auto loadedListItem = std::make_unique<QListWidgetItem>(); //Creates a new widget item
+        auto loadedItem = std::make_unique<stockItem>(); //Creates a new stock item
+
+        loadedItem->itemName->setText(stockItemName); //Sets the stock item name as the split name
+        loadedItem->priceSpinBox->setValue(stockItemPrice); //Sets the stock item price as the converted float value
+        loadedItem->spinBox->setValue(stockItemAmount); //Sets the stock item amount as the converted int value
+
+        this->stock.push_back(*loadedItem); //Adding a new element to a vector
+
+        loadedListItem->setSizeHint(loadedItem->stockItemWidget->sizeHint());
+
+        ui.itemList->addItem(loadedListItem.release()); //Adds the item to the list
+        ui.itemList->setItemWidget(ui.itemList->item(ui.itemList->count() - 1), loadedItem->stockItemWidget);
     }
+
+    file.close(); //Closes the file
 }
 
 //Saving current stock to file
